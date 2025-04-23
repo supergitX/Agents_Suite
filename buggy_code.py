460f746f739ea790613b3c087e6f9d26ca7693dc
Naveen Nidadavolu <115283282+navi004@users.noreply.github.com>
2025-04-24 00:03:32 +0530

    Create buggy_code.py

---
+++ buggy_code.py
@@ -0,0 +1,11 @@
+def divide_numbers(a, b):
+    return a / b
+
+def is_even(num):
+    if num % 2 = 0:
+        return True
+    else:
+        return False
+
+def get_element_at_index(lst, index):
+    return lst(index)
